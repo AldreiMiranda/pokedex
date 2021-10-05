613aec0dc799bc91d6b5b66feb9158a38fb99d82
aldreiMiranda <aldrei_miranda@hotmail.com>
2021-10-05 01:44:38 -0300

    feat(home): css

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,9 +9,50 @@ export default createGlobalStyle`
 
 
 body {
-    background: ${props => props.theme.colors.background};
-    color: ${props => props.theme.colors.text};
-    font: 400 16px Roboto, sans-serif;
+    margin: 0;
+    width: 100%;
+    height: 100vh;
+    font-family: "Exo", sans-serif;
+    color: #fff;
+    background: linear-gradient(90deg, #ffe4eb, #cbeaf4, #ffe4eb, #cbeaf4);
+    background-size: 400% 400%;
+    -webkit-animation: gradientBG 12s ease infinite forwards;
+    animation: gradientBG 12s ease infinite forwards;
+  }
+  .container {
+    width: 100%;
+    position: absolute;
+    top: 35%;
+    text-align: center;
+  }
+  h1 {
+    font-weight: 900;
+  }
+  a {
+    text-decoration: none;
+    color: #212121;
+  }
+  @-webkit-keyframes gradientBG {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+  @keyframes gradientBG {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
 }
 
 `
