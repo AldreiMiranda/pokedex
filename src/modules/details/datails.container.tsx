b5ab259c8b38b01e37aedfeb554a0511aec0eb21
aldreiMiranda <aldrei_miranda@hotmail.com>
2021-12-07 00:39:54 -0300

    feat(inst): new modal details

--- src/modules/details/datails.container.tsx
+++ src/modules/details/datails.container.tsx
@@ -1,10 +1,10 @@
 import { Grid, Box, Typography } from '@mui/material'
-import { BoxCard, ContaiberBox } from './details.style'
+import { BoxCard, ContainerBox, BoxGrid, Types } from './details.style'
 import Image from 'next/image'
 
 export const Details = () => {
     return (
-        <ContaiberBox>
+        <ContainerBox>
             <Grid container>
                 <Grid xs={6}>
                     <BoxCard>
@@ -16,15 +16,111 @@ export const Details = () => {
                         />
                     </BoxCard>
                 </Grid>
-                <Grid xs={6}>
+                <Grid xs={6} alignSelf="center">
                     <Box>
-                        <Typography color="black" variant="subtitle1">
+                        <Typography color="black" variant="h5">
                             On the night of a full moon, if shadows move on
                             their own and laugh, it must be Gengar’s doing
                         </Typography>
                     </Box>
+                    <BoxGrid>
+                        <Grid container>
+                            <Grid xs={6}>
+                                <Box display="flex" alignItems="baseline">
+                                    <Typography color="black" variant="h6">
+                                        Height:
+                                    </Typography>
+                                    <Typography
+                                        color="black"
+                                        variant="body1"
+                                        marginLeft="5px"
+                                    >
+                                        1.5M
+                                    </Typography>
+                                </Box>
+                            </Grid>
+                            <Grid xs={6}>
+                                <Box display="flex" alignItems="baseline">
+                                    <Typography color="black" variant="h6">
+                                        Category:
+                                    </Typography>
+                                    <Typography
+                                        color="black"
+                                        variant="body1"
+                                        marginLeft="5px"
+                                    >
+                                        Shadow
+                                    </Typography>
+                                </Box>
+                            </Grid>
+                            <Grid xs={6}>
+                                <Box display="flex" alignItems="baseline">
+                                    <Typography color="black" variant="h6">
+                                        weight:
+                                    </Typography>
+                                    <Typography
+                                        color="black"
+                                        variant="body1"
+                                        marginLeft="5px"
+                                    >
+                                        40.5kg
+                                    </Typography>
+                                </Box>
+                            </Grid>
+                            <Grid xs={6}>
+                                <Box display="flex" alignItems="baseline">
+                                    <Typography color="black" variant="h6">
+                                        Abilities:
+                                    </Typography>
+                                    <Typography
+                                        color="black"
+                                        variant="body1"
+                                        marginLeft="5px"
+                                    >
+                                        Cursed Body
+                                    </Typography>
+                                </Box>
+                            </Grid>
+                            <Grid xs={6}>
+                                <Box display="flex" alignItems="baseline">
+                                    <Typography color="black" variant="h6">
+                                        Gender:
+                                    </Typography>
+                                    <Typography
+                                        color="black"
+                                        variant="body1"
+                                        marginLeft="5px"
+                                    ></Typography>
+                                </Box>
+                            </Grid>
+                        </Grid>
+                    </BoxGrid>
+                </Grid>
+                <Grid xs={6}>
+                    <Box>
+                        <Typography color="black" variant="h5">
+                            Type
+                        </Typography>
+                        <Box>
+                            <Types color={'#7b62a3'}>Ghost</Types>
+                            <Types color={'#b97fc9'}>Poison</Types>
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid xs={6}>
+                    <Box>
+                        <Typography color="black" variant="h5">
+                            Weaknesses
+                        </Typography>
+                        <Box>
+                            <Types color={'#7b62a3'}>Ghost</Types>
+                            <Types color={'#707070'}>Dark</Types>
+                            <Types color={'#f366b9'}>Psychic</Types>
+                            <Types color={'#f7de3f'}>Ground</Types>
+                        </Box>
+                    </Box>
                 </Grid>
             </Grid>
-        </ContaiberBox>
+        </ContainerBox>
     )
 }
--- src/modules/details/details.style.ts
+++ src/modules/details/details.style.ts
@@ -5,12 +5,30 @@ export const BoxCard = styled.div`
     &:active,
     &:focus {
         outline: none;
-        transform: scale(1.1);
+        transform: scale(1.2);
     }
 `
 
-export const ContaiberBox = styled.div`
+export const ContainerBox = styled.div`
     padding-left: 5%;
     padding-right: 5%;
-    margin-top: 50px;
+    margin-top: 40px;
+`
+
+export const BoxGrid = styled.div`
+    margin-top: 30px;
+    padding: 20px;
+    background-color: #d7fffe;
+    box-shadow: 4px 4px rgba(0, 0, 0, 0.1);
+    border-radius: 8px;
+`
+
+export const Types = styled.div`
+    background-color: ${props => props.color};
+    padding: 5px 10px;
+    border-radius: 5px;
+    font-size: 18px;
+    margin-top: 10px;
+    width: 200px;
+    text-align: center;
 `
